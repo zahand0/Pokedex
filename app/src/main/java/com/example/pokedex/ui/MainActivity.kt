b0c9215da780f83bbc7939275d220780bb8f871d
Sergey <mulichev@gmail.com>
2022-10-19 16:47:11 +0300

    moved to ui package

--- app/src/main/java/com/example/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivity.kt
@@ -17,7 +17,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.example.pokedex.pokemonlist.PokemonListScreen
+import com.example.pokedex.ui.pokemonlist.PokemonListScreen
 import com.example.pokedex.ui.theme.PokedexTheme
 import dagger.hilt.android.AndroidEntryPoint
 
