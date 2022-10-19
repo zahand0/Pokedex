f0319f9b1a880eeb3b951412bb8bfef528c49e72
Sergey <mulichev@gmail.com>
2022-10-19 16:48:01 +0300

    moved to ui package

--- app/src/main/java/com/example/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivity.kt
@@ -1,15 +1,10 @@
-package com.example.pokedex
+package com.example.pokedex.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
@@ -31,7 +26,7 @@ class MainActivity : ComponentActivity() {
                 NavHost(
                     navController = navController,
                     startDestination = "pokemon_list_screen"
-                ){
+                ) {
                     composable("pokemon_list_screen") {
                         PokemonListScreen(navController = navController)
                     }
